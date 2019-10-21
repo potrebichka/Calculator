1860fd21637a4438b45ad250d6d77ef2ebd1c335
Nina Potrebich and Sacide Basol <sacide@>
2019-10-21 13:30:41 -0700

    add nnew changes

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var add= function(number1, number2){
+return number1 + number2;
+};
+
+var substract= function(number1, number2){
+  return number1 - number2;
+}
+
+var multiply = function(number1, number2){
+  return number1 * number2;
+
+}
+
+var divide= function(number1 , number2){
+  return number1 / number2;
+
+}
+var number1= parseInt(prompt("Enter a number:"));
+var number2= parseInt(prompt('Enter another number:'));
+
+var result = divide(number1, number2);
+alert(result);
